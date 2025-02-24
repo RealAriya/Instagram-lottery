80706d3d4530ec8253ece4bc47f28d5204ff716d
Arya <arya.t.innovate@gmail.com>
2025-02-24 14:14:35 +0330

    feat: add log in and lottery choise form with validation

--- forms.py
+++ forms.py
@@ -3,10 +3,29 @@ from wtforms import StringField, PasswordField, SubmitField , SelectField
 from wtforms.validators import DataRequired, Email, EqualTo, Length
 
 
-# Handles user registration.
+# Sign up
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=12)])
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     confrim_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign up')
+
+# Log in
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
+
+
+# Lottery choise
+# Allows users to select a lottery type.
+
+class LotteryChoiceForm(FlaskForm):
+    lottery_type = SelectField('Select Lottery Type', choices=[
+        ('comments', 'Lottery by Comments'),
+        ('likes', 'Lottery by Likes'),
+        ('score', 'Lottery by Score'),
+        ('followers', 'Lottery by Followers')
+    ])
+    submit = SubmitField('Proceed')
