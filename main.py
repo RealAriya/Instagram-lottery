9a16b70ae95c2821e9985eff061996130cdb9b8f
Arya <arya.t.innovate@gmail.com>
2025-02-25 16:48:21 +0330

    feat: add start_lottery route
    + import modules
    + List participants
    + filter comments and  mentions
    + collect likes, followers
    + check if the number of
       participants was more that
      100 redirecy to payment.
    + call Excel generator

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from flask import Flask, render_template, redirect, url_for, request, flash
 from models import db, bcrypt, Users, LotteryType
 from forms import RegistrationForm, LoginForm, LotteryChoiceForm
+from data_scraper import login, collect_comments, collect_likes, collect_followers
 import os
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+import random
+from payment import process_payment
+from excel_generator import generate_excel
 
 
 
@@ -86,3 +92,55 @@ def choose_lottery():
             flash('There was an Error while choosing lottery type', 'danger') 
 
     return render_template('choose_lottery.html', form=form)
+
+
+
+@app.route('/start_lottery/<lottery_type>', methods=['GET', 'POST'])
+def start_lottery(lottery_type):
+    form = LotteryChoiceForm()
+    if form.validate_on_submit():
+        post_url = request.form['post_url']
+        min_comments = form.min_comments.data
+        min_mentions = form.min_mentions.data
+
+        # Initialize ChromeDriver
+        service = Service('/usr/bin/chromedriver')  
+        driver = webdriver.Chrome(service=service)
+        login(driver)
+
+        participants = []
+        if 'comments' in lottery_type:
+            comments = collect_comments(driver, post_url)
+            filtered_comments = {
+                user: data for user, data in comments.items()
+                if data['comment_count'] >= min_comments and data['mention_count'] >= min_mentions
+            }
+            participants.extend(list(filtered_comments.keys()))
+        if 'likes' in lottery_type:
+            likes = collect_likes(driver, post_url)
+            participants.extend(likes)
+        if 'followers' in lottery_type:
+            followers = collect_followers(driver, post_url)
+            participants.extend(followers)
+
+        driver.quit()
+
+        # Check if payment is required
+        if len(participants) >= 100:  
+            payment_url = process_payment(amount=1000, callback_url=url_for('lottery_result', _external=True))
+            if payment_url:
+                return redirect(payment_url)
+            else:
+                flash('Payment processing failed. Please try again.', 'danger')
+                return redirect(url_for('start_lottery', lottery_type=lottery_type))
+
+        # Save the participant data to an Excel file:
+        generate_excel({"username": participants}, 'participants.xlsx')
+
+        # Select a winner
+        winner = random.choice(participants)
+        return redirect(url_for('lottery_result', winner=winner))
+
+    return render_template('lottery.html', form=form, lottery_type=lottery_type)
+
+
